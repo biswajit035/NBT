cad670a99a6421e03c956244ac5296eb176fa77a
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-02-04 13:08:50 +0530

    feat: :sparkles: company sllider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-import About from "@/pages/About";
-import Advantages from "@/pages/Advantages";
-import CompanySlider from "@/pages/CompanySlider";
-import Faq from "@/pages/Faq";
-import Footer from "@/pages/Footer";
-import Hero from "@/pages/Hero";
-import Navbar from "@/pages/Navbar";
-import Services from "@/pages/Services";
-import Team from "@/pages/Team";
-import Testimonial from "@/pages/Testimonial";
+import About from "@/src/pages/About";
+import Advantages from "@/src/pages/Advantages";
+import CompanySlider from "@/src/pages/CompanySlider";
+import Faq from "@/src/pages/Faq";
+import Footer from "@/src/pages/Footer";
+import Hero from "@/src/pages/Hero";
+import Navbar from "@/src/pages/Navbar";
+import Services from "@/src/pages/Services";
+import Team from "@/src/pages/Team";
+import Testimonial from "@/src/pages/Testimonial";
 
 export default function Home() {
   return (
     <main>
         <Navbar/>
          <Hero/>
-        {/*
         <CompanySlider/>
+        {/*
         <About/>
         <Services/>
         <Advantages/>
