cbf60b85a6fa9d24df284020eea3b7efe76e0dd5
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-02-06 23:12:11 +0530

    fix: :bug: style fixed

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -22,11 +22,11 @@ const Team = () => {
   ]
 
   return (
-    <div className='relative h-fit w-full overflow-x-hidden px-2 py-5 md:px-10 lg:px-20'>
-      <div className='absolute -right-1 z-[1] hidden aspect-square rounded-full border border-[#2b2b2b] bg-gridBg hover:bg-[#1c1c1c] md:block md:w-[45vw] lg:w-[20vw]'></div>
+    <div className='relative h-fit w-full overflow-x-hidden px-2 py-5 md:px-10 md:pt-20 lg:px-20'>
+      <div className='absolute -left-20 top-2 z-[1] hidden aspect-square rounded-full border border-[#2b2b2b] bg-gridBg hover:bg-[#1c1c1c] md:block md:w-[45vw] lg:-left-10 lg:w-[20vw]'></div>
       <div className="relative z-[2]">
-        <div className='mb-10 w-fit rounded-lg bg-[#1f1f1f] px-10 py-1 text-xl'>Team</div>
-        <h1 className='mb-8 text-center text-3xl md:text-5xl lg:text-6xl'>Meet Our </h1>
+        <div className='heading'>Team</div>
+        <h1 className='mb-2 text-center text-3xl md:text-5xl lg:text-6xl'>Meet Our </h1>
         <h1 className='mb-8 text-center text-3xl md:text-5xl lg:text-6xl'>Professionals</h1>
       </div>
       <div className='relative z-[3] flex w-full flex-col gap-5 lg:flex-row'>
